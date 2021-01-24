7f95ce6001d52e463dd982a5a4cd023110e7068d
Mehmet Görer <mehmetgorer@gmail.com>
2021-01-24 14:14:09 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enlish to French dictionary");
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("One", "Un");
+            myDictionary.Add("Two", "Deux");
+            myDictionary.Add("Three", "Trois");
+            myDictionary.Add("Four", "Uatre");
+            myDictionary.Add("Five", "Cinq");
+
+            myDictionary.Read();
+
+        }
+    }
+}
